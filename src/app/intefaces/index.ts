f9a40c5c22ac635245777a1efd112e67597364d3
LeonidPanov <leonardopanov@yahoo.com>
2022-05-15 19:50:05 +0300

    hw2

---
+++ src/app/intefaces/index.ts
@@ -0,0 +1,6 @@
+export * from './user.interface';
+export * from './post.interface';
+export * from './comment.interface';
+export * from './address.interface';
+export * from './geo.interface';
+export * from './company.interface';
