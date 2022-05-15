394a428702db85084638ff4d2fb7e8dcfd1d076f
LeonidPanov <leonardopanov@yahoo.com>
2022-05-15 16:21:32 +0300

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {HttpClientModule} from "@angular/common/http";
 import {AppComponent} from './app.component';
+import { UsersComponent } from './component/users/users/users.component';
+import { UserComponent } from './component/user/user/user.component';
+import { PostsComponent } from './component/posts/posts/posts.component';
+import { PostComponent } from './component/post/post/post.component';
+import { CommentsComponent } from './component/comments/comments/comments.component';
+import { CommentComponent } from './component/comment/comment/comment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    PostsComponent,
+    PostComponent,
+    CommentsComponent,
+    CommentComponent
   ],
   imports: [
     BrowserModule,
