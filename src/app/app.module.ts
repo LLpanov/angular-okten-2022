a0b0799872eacb9caa519abc73ead00e22f0470b
LeonidPanov <leonardopanov@yahoo.com>
2022-05-17 13:33:00 +0300

    hw2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
-import { UsersComponent } from './components/users/users.component';
-import { UserComponent } from './components/user/user.component';
+import {RouterModule} from "@angular/router";
+
+import {UserComponent} from './components/user/user.component';
+import {UsersComponent} from './components/users/users.component';
+import {UserDetailsComponent} from './components/user-details/user-details.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    UserComponent
+    UserComponent,
+    UserDetailsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
