78ee4e258e6291971419b7352aee10fd563ef6ba
LeonidPanov <leonardopanov@yahoo.com>
2022-05-22 19:31:52 +0300

    hw3

--- src/app/modules/index.ts
+++ src/app/modules/index.ts
@@ -3,3 +3,4 @@ export * from './post/post.module';
 export * from './comment/comment.module';
 export * from './todo/todo.module';
 export * from './album/album.module';
+export * from './photo/photo.module';
