706195fdfd12d6092219589265c32049381d8f66
LeonidPanov <leonardopanov@yahoo.com>
2022-05-25 03:36:27 +0300

    hw4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from "./app-routing.module";
 import {HttpClientModule} from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home/home.component';
-import { LayoutComponent } from './layout/layout/layout.component';
-import {RouterModule} from "@angular/router";
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home/home.component';
+import {LayoutComponent} from './layout/layout/layout.component';
 
 
 @NgModule({
@@ -17,9 +17,10 @@ import {RouterModule} from "@angular/router";
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
