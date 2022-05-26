64436e4d33bafbdec4f2c5bda8b45e9771fc607f
LeonidPanov <leonardopanov@yahoo.com>
2022-05-26 15:01:55 +0300

    hw5

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule, Routes} from "@angular/router";
+
+import {LayoutComponent} from "./layout/layout/layout.component";
+
+const routes: Routes = [
+  {path:'',component:LayoutComponent,children:[]}
+]
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
+
+
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
